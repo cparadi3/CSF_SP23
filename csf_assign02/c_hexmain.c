a94db975a12442eed741dc40650455305d1db5c4
Joseph Zahner <jzahner4@ugrad5.cs.jhu.edu>
2023-02-13 18:03:35 -0500

    main working with basic tests

--- csf_assign02/c_hexmain.c
+++ csf_assign02/c_hexmain.c
@@ -15,18 +15,23 @@ int main(void) {
     hex_format_offset(counter, sbuf);
     hex_write_string(sbuf);
     hex_write_string(": ");
-    for (unsigned i = 0; i < read; i++) {
+    for (unsigned i = 0; i < 16; i++) {
+      if(i < read) {
       hex_format_byte_as_hex(data_buf[i], (sbuf));
       hex_write_string(sbuf);
       hex_write_string(" ");
-      hex_to_printable(data_buf[i]);
+      data_buf[i] = hex_to_printable(data_buf[i]);
       counter++;
+      }
+      else if(i >= read) {
+        hex_write_string("   ");
+      }
     }
     hex_write_string(" ");
+    data_buf[read] = '\0';
     hex_write_string(data_buf);
     hex_write_string("\n");
     read = hex_read(data_buf);
-
   }
   //print the "position" of the string in the input (should start at zero + go up by 16 per line)
     //hex_format_offset and hex_write_string should be called (and hex_to_printable)
