67aa66952d108b332cad9ee2304bfa9f12baf7e3
Joseph Zahner <jzahner4@ugrad5.cs.jhu.edu>
2023-02-12 23:50:57 -0500

    just added pseudo code for main

--- csf_assign02/c_hexmain.c
+++ csf_assign02/c_hexmain.c
@@ -3,5 +3,15 @@
 #include "hexfuncs.h"  // this is the only header file which may be included!
 
 int main(void) {
-  // TODO: implement the main function
+  // TODO:
+
+  //read 16 bytes from stdin (maybe do a while loop like while(hex_read == 16))
+    //hex_read
+  //print the "position" of the string in the input (should start at zero + go up by 16 per line)
+    //hex_format_offset and hex_write_string should be called (and hex_to_printable)
+  //for each 16 char's in the line, format as hex
+    //call format_byte_as_hex, hex_to_printable, and hex_write_string
+  //separate values appropriately by spaces, colons, and newlines.
+  
+
 }
