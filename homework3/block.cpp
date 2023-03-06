dd349f93440ab4b3ba7ebfb40bc6ac73bc5688e3
Joseph Zahner <jzahner4@ugrad5.cs.jhu.edu>
2023-03-06 17:05:53 -0500

    mostly stubs here

---
+++ homework3/block.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+#include "block.h"
+
+Block::Block() {
+    //todo : implement
+    //byteVector.push_back() = "abcdef12";
+}
+Block::~Block() {
+    //TODO: implement
+}
---
+++ homework3/cache.cpp
@@ -0,0 +1,12 @@
+#include "cache.h"
+#include <vector>
+#include "set.h"
+
+Cache::Cache(int numSets, int numBlocks, int numBytes) {
+    for (int i = 0; i < numSets; i++) {
+        for (int j = 0; j < numBlocks; j++)
+            //TODO: implement constructor
+            break;
+            //setVector.push_back(i*j);
+    }
+}
